3b284165312e0a013a938a0b29d47a12e03cd3fa
unknown <learn.mohammadadil@gmail.com>
2026-03-20 01:03:10 +0530

    provider added to the resource

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -1,12 +1,24 @@
-resource "azure_resource_group" "rg" {
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
   name = "rg-terraform"
   location = "eastus"   
 }
 
 resource "azure_storage_account" "jkstg" {
   name                     = "jenkinsstorageacc"
-  resource_group_name      = azure_resource_group.rg.name
-  location                 = azure_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
