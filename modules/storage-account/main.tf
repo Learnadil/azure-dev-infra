da33c80e6c8a680f1e9cc11755d4d2b86542e5c2
unknown <learn.mohammadadil@gmail.com>
2026-03-20 01:13:39 +0530

    changed the account name

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "jkstg" {
-  name                     = "adil-jenkins-stg-acc"
+  name                     = "adil-jkstgacc"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
