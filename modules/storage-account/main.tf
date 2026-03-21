fb13bcb439050a6468ee678959528d71e2899001
unknown <learn.mohammadadil@gmail.com>
2026-03-22 00:28:13 +0530

    Added the backend storage

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -4,6 +4,13 @@ terraform {
         source = "hashicorp/azurerm"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "azurerm_resource_group.rg.name"
+    storage_account_name = "azurerm_storage_account.jkstg.name"
+    container_name = "jktf-dev-backend"
+    key = "dev-terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
